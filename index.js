cbb4b32916158ef4fb5382cfa1653b795a4fd4fb
GOLFISHMANALEXIS <dhhdkalalo@gmail.com>
2022-07-14 12:18:46 -0500

    sifuncionadeta

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const { Deta } = require("deta");
 const express = require("express");
 const upload = require("express-fileupload");
-PORT = 5000;
+PORT = 3000;
 
 const app = express();
 
 app.use(upload());
 
-const deta = Deta("Project_Key");
+const deta = Deta("b0ei6rus_9YsU9imxXfJMfgsTpPBH9eP9ufJtSVV5");
 const drive = deta.Drive("images");
-
+//b0ei6rus
 app.get('/', (req, res) => {
     res.send(`
     <form action="/upload" enctype="multipart/form-data" method="post">
@@ -24,12 +24,12 @@ app.post("/upload", async (req, res) => {
     const img = await drive.put(name, {data: contents});
     res.send(img);
 });
-
 app.get("/download/:name", async (req, res) => {
     const name = req.params.name;
     const img = await drive.get(name);
     const buffer = await img.arrayBuffer();
     res.send(Buffer.from(buffer));
+    new Error(message);
 }); 
 
 app.listen(
